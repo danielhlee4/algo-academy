b262d2df850b874e53c71e2eb9eb33380ae9633f
Dan Lee <danielhlee4@gmail.com>
2023-12-08 13:46:07 -0500

    add contains duplicate II

--- arrays.js
+++ arrays.js
@@ -107,3 +107,25 @@ const maxArea = (height) => {
     return max;
 
 };
+
+// contains duplicate II
+
+const containsNearbyDuplicate = (nums, k) => {
+    const window = new Set();
+    let left = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        if (right - left > k) {
+            window.delete(nums[left]);
+            left++;
+        }
+
+        if (window.has(nums[right])) {
+            return true;
+        }
+
+        window.add(nums[right]);
+    }
+
+    return false;
+}
