1daf72e8363bd4b752fd7fed44dedbf10fdb9ec5
Dan Lee <danielhlee4@gmail.com>
2023-12-11 16:27:56 -0500

    add two sum II

--- arrays.js
+++ arrays.js
@@ -152,3 +152,20 @@ const lengthOfLongestSubstring = (s) => {
 
     return maxLength;
 };
+
+// two sum ii
+
+const twoSum = (numbers, target) => {
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while (left < right) {
+        if (numbers[left] + numbers[right] < target) {
+            left++;
+        } else if (numbers[left] + numbers[right] > target) {
+            right--;
+        } else {
+            return [left + 1, right + 1];
+        }
+    }
+};
