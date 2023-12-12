208dcf852bd78d041fca9be5da8a10b02a0323dc
Dan Lee <danielhlee4@gmail.com>
2023-12-12 11:39:34 -0500

    add minimum size subarray sum

--- arrays.js
+++ arrays.js
@@ -186,3 +186,31 @@ const romanToInt = (s) => {
 
     return total;
 };
+
+// Minimum Size Subarray Sum
+
+const minSubArrayLen = (target, nums) => {
+    if (nums.length === 0) {
+      return 0;
+    }
+  
+    let left = 0;
+    let right = 0;
+    let minLength = Infinity;
+    let sum = 0
+  
+    while (right < nums.length) {
+      sum += nums[right];
+  
+      while (sum >= target) {
+        minLength = Math.min(minLength, right - left + 1);
+        sum -= nums[left];
+        left++
+      }
+  
+      right++
+    }
+  
+    return minLength === Infinity ? 0 : minLength;
+  
+};
