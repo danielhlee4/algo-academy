f15fef2c3131aec741f4203d84520846212f717e
Dan Lee <danielhlee4@gmail.com>
2023-12-14 11:38:35 -0500

    add two sum rb

--- arrays.rb
+++ arrays.rb
@@ -1,2 +1,13 @@
 # two sum
 
+def two_sum(nums, target)
+    h = {}
+    nums.each_with_index do |num, i|
+        complement = target - num
+        if h[complement]
+            return [i, h[complement]]
+        else
+            h[num] = i
+        end
+    end
+end
