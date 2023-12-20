856b00af72da2c9b36cad53c7df75d58657e0865
Dan Lee <danielhlee4@gmail.com>
2023-12-20 14:34:05 -0500

    add set matrix zeroes

--- hashing.js
+++ hashing.js
@@ -13,3 +13,25 @@ const twoSum = (nums, target) => {
         }
     }
 };
+
+// set matrix zeroes
+
+const setZeroes = matrix => {
+    const rowSet = new Set();
+    const colSet = new Set();
+
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[0].length; col++)
+        if (matrix[row][col] === 0) {
+            rowSet.add(row);
+            colSet.add(col);
+        }
+    }
+
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[0].length; col++)
+        if (rowSet.has(row) || colSet.has(col)) {
+            matrix[row][col] = 0;
+        }
+    }
+};
