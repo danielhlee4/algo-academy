a8e101b9ffef6100d16806e11ebb79e0d429b08e
Dan Lee <danielhlee4@gmail.com>
2024-01-04 20:40:26 -0500

    add sort array

--- sorting.js
+++ sorting.js
@@ -14,3 +14,29 @@ const sortColors = nums => {
         }
     }
 };
+
+// sort array (merge sort)
+
+const sortArray = nums => {
+    if (nums.length < 2) return nums;
+
+    const mid = Math.floor(nums.length / 2);
+    const left = sortArray(nums.slice(0, mid));
+    const right = sortArray(nums.slice(mid));
+
+    return merge(left, right);
+};
+
+const merge = (left, right) => {
+    const merged = [];
+
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            merged.push(left.shift());
+        } else {
+            merged.push(right.shift())
+        }
+    }
+
+    return merged.concat(left).concat(right);
+}
