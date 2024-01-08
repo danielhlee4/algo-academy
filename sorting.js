2b25fc7e5fa6cfdbac28bc7c92cceb6d091ed961
Dan Lee <danielhlee4@gmail.com>
2024-01-08 16:19:52 -0500

    add binary search iterative

--- sorting.js
+++ sorting.js
@@ -98,3 +98,23 @@ const frequencySort = s => {
 
     return res;
 };
+
+// iterative b search
+
+const search = (nums, target) => {
+    let [left, right] = [0, nums.length - 1];
+
+    while (left <= right) {
+        let mid = left + Math.floor((right - left) / 2)
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return -1;
+};
